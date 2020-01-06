c6b169d7cf17c7a732f208be9f9c31f75b4612fe
Cristian Jativa <cristian.jativa@kfc.com.ec>
2020-01-06 11:47:26 -0500

    mapas actualizados

--- mapasGoogle/src/app/app.module.ts
+++ mapasGoogle/src/app/app.module.ts
@@ -5,12 +5,11 @@ import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MapasComponent } from './mapas/mapas.component';
-<<<<<<< HEAD
-=======
+
 
 
 const config: SocketIoConfig = { url: 'http://localhost:5000' };
->>>>>>> 74c619557f1a7709d4dc2112e8e50385e2ec108c
+
 
 @NgModule({
   declarations: [
